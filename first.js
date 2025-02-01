c705d01da5a389f33b93f4aa7e053d179299e07c
priyanshujain5 <jainpriyanshu009@gmail.com>
2025-02-01 14:15:51 +0530

    js added

---
+++ first.js
@@ -0,0 +1,17 @@
+import React,{useState,useEffect} from "react";
+import ReactDOM from "react-dom/client"
+import CryptoBody from "./component/body"
+
+function Main(){
+
+    return (
+        <>
+        <div className="headers">
+            <h1>Welcome to crypto world where you get real time update of the most hyped currency in this universe</h1>
+        </div>
+        <CryptoBody></CryptoBody>
+        </>
+    )
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Main></Main>)
