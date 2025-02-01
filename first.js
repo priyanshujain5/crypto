d096aa5f2a9607b0f44a40349b343fd480a10377
priyanshujain5 <jainpriyanshu009@gmail.com>
2025-02-01 14:18:53 +0530

    Update first.js

--- first.js
+++ first.js
@@ -1,6 +1,6 @@
 import React,{useState,useEffect} from "react";
 import ReactDOM from "react-dom/client"
-import CryptoBody from "./component/body"
+import CryptoBody from "body"
 
 function Main(){
 
